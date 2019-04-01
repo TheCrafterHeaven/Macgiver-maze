b5b5d18574072a124635c1c3d88138618cc22ed4
Etienne <etiennr472@gmail.com>
2019-04-01 14:51:30 +0200

    Add Readme add Maze file and add pictures

---
+++ README.txt
@@ -0,0 +1,10 @@
+Ce jeu à pour but de vous divertir.
+Il est question d'aider McGiver à sortir d'un labyrinthe. En ramassant des objets vous pourrez tromper les gardien.
+Bon courrage !
+
+Guide d'utilisation : 
+
+
+Gameplay :
+
+
---
+++ constantes.py
@@ -0,0 +1,23 @@
+# Window settings
+SPRITE_NUM = 15  # Number of sprites per side of the Maze.
+SPRITE_SIZE = 40  # Dimension in pixels of the sprite size.
+WINDOW_DIM = SPRITE_SIZE * SPRITE_NUM  # Dimension in pixels of the Maze.
+
+# window title
+WINDOW_TITLE = "MacGyver Maze - have fun !"
+# Window picture
+WINDOW_PIC = "pictures/MacGyver.png"
+# Picture of the maze Background
+BACKGROUND = "pictures/background.png"
+# Home window
+Home_picture = "pictures/Home.PNG"
+# Loading the pictures representing the Maze.
+Wall_Picture = "pictures/wall.png" # Wall tiles.
+Departure_Picture = "pictures/departure.png" # First sprite
+Guardian_Picture = "pictures/Gardien.png" # Guardian = freedom place.
+
+# Loading the pictures of MacGyver and the 3 items
+macgyver_picture = "pictures/MacGyver.png"
+needle_picture = "pictures/needle.png"
+tube_picture = "pictures/plastic_tube.png"
+ether_picture = "pictures/ether.png"
