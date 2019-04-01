309d0bfcc5ce714f5372c8dfd45522fa51ce279f
Etienne <etiennr472@gmail.com>
2019-04-01 15:29:48 +0200

    begining Main with firsts loops and home page

--- Constants.py
+++ Constants.py
@@ -1,23 +1,23 @@
-# Window settings
+# window parameters
+WINDOW_TITLE = 'Macgiver Maze'
 SPRITE_NUM = 15  # Number of sprites per side of the Maze.
 SPRITE_SIZE = 40  # Dimension in pixels of the sprite size.
 WINDOW_DIM = SPRITE_SIZE * SPRITE_NUM  # Dimension in pixels of the Maze.
 
-# window title
-WINDOW_TITLE = "MacGyver Maze - have fun !"
-# Window picture
-WINDOW_PIC = "pictures/MacGyver.png"
 # Picture of the maze Background
 BACKGROUND = "pictures/background.png"
+
 # Home window
-Home_picture = "pictures/Home.PNG"
+HOME_PICTURE = "pictures/Home.PNG"
+WIN_PICTURE = "pictures/YouWin.png"
+LOSE_PICTURE = "pictures/GameOver.png"
+
 # Loading the pictures representing the Maze.
-Wall_Picture = "pictures/wall.png" # Wall tiles.
-Departure_Picture = "pictures/departure.png" # First sprite
-Guardian_Picture = "pictures/Gardien.png" # Guardian = freedom place.
+WALL_PICTURE = "pictures/wall.png" # Wall tiles.
+GUARDIAN_PICTURE = "pictures/Gardien.png" # Guardian = freedom place.
 
 # Loading the pictures of MacGyver and the 3 items
-macgyver_picture = "pictures/MacGyver.png"
-needle_picture = "pictures/needle.png"
-tube_picture = "pictures/plastic_tube.png"
-ether_picture = "pictures/ether.png"
+MACGIVER_PICTURE = "pictures/MacGyver.png"
+NEEDLE_PICTURE = "pictures/needle.png"
+TUBE_PICTURE = "pictures/plastic_tube.png"
+ETHER_PICTURE = "pictures/ether.png"
--- Main.py
+++ Main.py
@@ -7,3 +7,56 @@ With those objects you can asleep the guardian and success
 
 Files :
 Constants.py, Main.py, Maze_Struct.txt, pictures, README.md"""
+
+import pygame
+import Constants
+
+pygame.init()
+
+# Opening pygame window with sizes (width, height)
+window = pygame.display.set_mode((Constants.WINDOW_DIM, Constants.WINDOW_DIM))
+
+icon = pygame.image.load(Constants.MACGIVER_PICTURE)
+pygame.display.set_icon(icon)
+
+# Title
+pygame.display.set_caption(Constants.WINDOW_TITLE)
+
+# First loop
+first_loop = 1
+while first_loop:
+    # loading the home screen
+    home = pygame.image.load(Constants.HOME_PICTURE).convert()
+    window.blit(home, (0, 0))
+
+    # Refresh
+    pygame.display.flip()
+
+    game_loop = 1
+    home_loop = 1
+
+    # home loop
+    while home_loop:
+
+        pygame.time.Clock().tick(30)
+
+        for event in pygame.event.get():
+
+            # if the user exit the game, we put variables to 0
+            # that close the window
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+
+                first_loop = 0
+                game_loop = 0
+                home_loop = 0
+                # introducing variable to start the game
+                go = 0
+
+            elif event.type == pygame.KEYDOWN:
+                # playing game loop
+                if event.key == pygame.K_F1:
+                    home_loop = 0
+                    go = "Maze.Struct.txt"
+
+    if go != 0:
+        background == pygame.image.load(Constants.BACKGROUND).convert()
